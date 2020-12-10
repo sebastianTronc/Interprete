ea5e8fa414d3f0d95369344c9bcaef62ae37e2c7
Sebastian T <sebas.flow.16.22@gmail.com>
2020-12-09 23:43:33 -0500

    Iniciando pruebas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import peg from "pegjs"
+import lenguaje from "./lenguaje.pegjs"
+import lenguaje2 from "./lenguaje.txt"
+import lenguajeN from "./lenguaje"
 
 function App() {
+ /* var leng=`
+  Expression
+  = head:Term tail:(_ ("+" / "-") _ Term)* {
+      return tail.reduce(function(result, element) {
+        if (element[1] === "+") { return result + element[3]; }
+        if (element[1] === "-") { return result - element[3]; }
+      }, head);
+    }
+
+Term
+  = head:Factor tail:(_ ("*" / "/") _ Factor)* {
+      return tail.reduce(function(result, element) {
+        if (element[1] === "*") { return result * element[3]; }
+        if (element[1] === "/") { return result / element[3]; }
+      }, head);
+    }
+
+Factor
+  = "(" _ expr:Expression _ ")" { return expr; }
+  / Integer
+
+Integer "integer"
+  = _ [0-9]+ { return parseInt(text(), 10); }_
+
+
+  `*/
+
+  
+  const handleClick =()=>{
+    try {
+     // var parser=peg.generate("star=('a'/'b')+")
+      var texto= document.getElementById("it").value 
+      console.log( lenguajeN.parse(texto) ) 
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="textarea" id="it"/>
+      <button onClick={handleClick}>Jorge cachón.</button>
     </div>
   );
 }
